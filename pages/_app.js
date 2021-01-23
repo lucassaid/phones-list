310f0ce48be1f939ec472a6cb216ded3104009ef
Lucas Said <lsaid@spacedev.uy>
2021-01-23 20:56:58 -0300

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -18,15 +18,13 @@ function MyApp({ Component, pageProps }) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/list.svg"
           rel="icon"
-          type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/list.svg"
           rel="icon"
-          type="image/png"
           sizes="32x32"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
