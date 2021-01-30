66c5dd5635839ef3e0fdee7b11ea84ba1d48a286
Lucas Said <lsaid@spacedev.uy>
2021-01-30 18:37:26 -0300

    added delete feature

--- components/SequenceItem.js
+++ components/SequenceItem.js
@@ -13,13 +13,13 @@ export default function SequenceItem({ sequenceId, range, onDelete }) {
     <div className="flex w-full items-center justify-end rounded-lg my-3 bg-gray-100 py-2 px-4">
       <div className="mr-auto">{legibleRange}</div>
       <Link href={`/${sequenceId}`}>
-        <a className="ml-4">
+        <a className="ml-4 mr-2">
           <button className="btn-small border">
             Continuar
           </button>
         </a>
       </Link>
-      {/* <DeleteSequenceModal onDelete={onDelete}/> */}
+      <DeleteSequenceModal onDelete={onDelete}/>
     </div>
   )
 }
