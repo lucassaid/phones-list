7b8b6f0166191ebf55070a015418778b844a2ac5
Lucas Said <lsaid@spacedev.uy>
2021-02-13 10:49:58 -0300

    refactoring

--- components/NumbersList/Progress.js
+++ components/NumbersList/Progress.js
@@ -7,21 +7,23 @@ export default function Progress({numbersArr}) {
     return numbersArr.filter(number => number.called).length
   }, [numbersArr])
 
-  const seeStatistics = (
-    <button style={{display: 'flex'}} className="btn border items-center"  >
-      <span>
-        Ver estadísticas
-      </span>
-    </button>
-  )
-
   return (
     <div className="fixed w-full bottom-0 left-0">
       <div className="max-w-lg w-full mx-auto bg-blue-500 h-15 py-2 text-white flex items-center justify-center md:rounded-t-lg">
         <div className="mr-10">
           Avance: {calledNumbers} de {numbersArr.length}  
         </div>
-        <StatisticsModal trigger={seeStatistics} numbersArr={numbersArr}  /> 
+        <StatisticsModal
+          trigger={(
+            <button
+              className="btn-small border"
+              disabled={!calledNumbers}
+            >
+              Estadísticas
+            </button>
+          )}
+          numbersArr={numbersArr}
+        /> 
       </div>
     </div>
   )
--- components/NumbersList/StatisticsModal.js
+++ components/NumbersList/StatisticsModal.js
@@ -2,55 +2,29 @@ import React from 'react'
 import { useAlert } from '../Alert'
 import { getStatistics } from '../../lib/utils'
 
-const defaultTrigger = (
-  <button className="btn-small border">
-    Estadísticas
-  </button>
-)
-
 const DatesList = ({datesData}) => (
-  <div className="mt-6">
-    {datesData.existenceDates.map(date => (
-        <div key={date} className="my-2">
-          <span className="opacity-70">{date}</span>
-          <span className="mx-3">-</span>
-          {datesData.allDates[date]} llamadas
-        </div>
-      )
-    )}
+  <div className="mt-6 max-w-xxs mx-auto">
+    {datesData.map(({date, quantity}) => (
+      <div key={date} className="my-2">
+        <span className="opacity-70">{date}</span>
+        <span className="mx-3">-</span>
+        {quantity} llamadas
+      </div>
+    ))}
   </div>
 )
 
-export default function StatisticsModal({trigger = defaultTrigger, numbersArr, }) {
+export default function StatisticsModal({ trigger, numbersArr }) {
   
-  const { alert, closeAlert } = useAlert()
+  const { alert } = useAlert()
    
   const openStatistics = () => {
     const datesData = getStatistics(numbersArr)
     alert({
-      title: 'Llamadas realizadas ordenadas por fecha',
-      content: (
-        <>
-          {datesData.existenceDates.length ? (
-            <DatesList datesData={datesData}/>
-          ) : (
-            <div className="opacity-60 text-center mt-4">
-              No hay llamadas realizadas
-            </div>
-          )}
-          <div className="text-right mt-4">
-            <button
-              className="btn btn-primary"
-              onClick={closeAlert}
-              tabIndex="4"
-            >
-              Ok
-            </button>
-          </div>
-        </>
-      )
+      title: 'Llamadas realizadas por día',
+      desc: <DatesList datesData={datesData}/>
     })
   }
-
-  return React.cloneElement(trigger,  {onClick: openStatistics})
+  
+  return React.cloneElement(trigger, {onClick: openStatistics})
 }
